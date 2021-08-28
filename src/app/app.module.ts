a15b8a465fd8ce930764c7d4a43ac6c0299374bc
Shakikhanli <shekikhanli@gmail.com>
2021-08-28 14:45:24 +0400

    Creating empty components for all major pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { PeopleComponent } from './people/people.component';
 import { PersonComponent } from './people/person/person.component';
 import { AssestListComponent } from './shared/assest-list/assest-list.component';
 import { ActivityListComponent } from './shared/activity-list/activity-list.component';
+import { PlacesComponent } from './places/places.component';
+import { BooksComponent } from './books/books.component';
+import { GamesComponent } from './games/games.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { ActivityListComponent } from './shared/activity-list/activity-list.comp
     PeopleComponent,
     PersonComponent,
     AssestListComponent,
-    ActivityListComponent
+    ActivityListComponent,
+    PlacesComponent,
+    BooksComponent,
+    GamesComponent,
+    StatisticsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
